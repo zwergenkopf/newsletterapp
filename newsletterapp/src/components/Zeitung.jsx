752775d865dc6deb890c70ff480d56b1783d9ca2
mariuspentz <pentzmarius@gmail.com>
2023-09-29 13:42:56 +0200

    123123

--- newsletterapp/src/components/Zeitung.jsx
+++ newsletterapp/src/components/Zeitung.jsx
@@ -1,15 +1,12 @@
-import { useEffect } from "react";
 
 function Zeitung() {
-    useEffect(() => {
-        getZeitung();
-    },[]);
+
     const getZeitung = async () => {
     const api = await fetch(
         `https://mc2xsctbllc5pw-0ck06pnh5c6g8.pub.sfmc-content.com/qusszr1p1zx`
     );
     const data = await api.json();
-    console.log(data);
+    console.log("123");
   }; 
   return (
     <div>Zeitung</div>
