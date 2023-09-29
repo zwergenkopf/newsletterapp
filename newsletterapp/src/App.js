31145d1a4cf7ff1e4740c9240556db9969919580
mariuspentz <pentzmarius@gmail.com>
2023-09-29 12:47:19 +0200

    123

--- newsletterapp/src/App.js
+++ newsletterapp/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 12345
         </a>
       </header>
     </div>
